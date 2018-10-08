44c7092f9f2aba2b8f5db215d4bab59b5ef1ff8b
Michal Szmyd <michal.szmyd@railwaymen.org>
2018-10-08 11:09:36 +0200

    Extended capybara specs

--- spec/features/work_time_spec.rb
+++ spec/features/work_time_spec.rb
@@ -7,11 +7,11 @@ describe 'signs me in, view projects, accounting_periods, timesheet', type: :fea
     end
   end
 
-  def login_user(user)
+  def login_user(user, with: 'password')
     visit '/users/sign_in'
     within('#new_user') do
       fill_in 'user[email]', with: user.email
-      fill_in 'user[password]', with: 'password'
+      fill_in 'user[password]', with: with
       click_button 'sign_in'
     end
   end
@@ -63,7 +63,6 @@ describe 'signs me in, view projects, accounting_periods, timesheet', type: :fea
   end
 
   it 'Timesheet' do
-    expect(Sidekiq).to receive(:redis).at_least(:once).and_return([])
     project = Project.create(name: 'test', active: true)
     Project.create(name: 'another', active: true)
     user = FactoryGirl.create :user, lang: 'en', password: 'password'
@@ -88,16 +87,92 @@ describe 'signs me in, view projects, accounting_periods, timesheet', type: :fea
 
     select_2_months_ago_tasks(work_time)
 
+    click_link('Sign out')
+    expect(page).to have_content('Zaloguj')
+  end
+
+  it 'Accounting Periods' do
+    expect(Sidekiq).to receive(:redis).at_least(:once).and_return([])
+    user = FactoryGirl.create :user, lang: 'en', password: 'password'
+    login_user(user)
+    FactoryGirl.create_list :accounting_period, 5, user: user
+
     click_link('Accounting Periods')
-    expect(page).to have_selector('table')
+
+    aggregate_failures 'contain table' do
+      expect(page).to have_selector('tbody > tr', count: 5)
+      expect(page).to have_selector('table')
+    end
+  end
+
+  it 'Projects' do
+    user = FactoryGirl.create :user, lang: 'en', password: 'password'
+    project = FactoryGirl.create :project, active: true, internal: false, name: 'Project30'
+    project2 = FactoryGirl.create :project, active: true, internal: false, name: 'Project60'
+    project3 = FactoryGirl.create :project, active: true, internal: false, name: 'Project90'
+    FactoryGirl.create :project, active: false, internal: false, name: 'Inactive'
+
+    FactoryGirl.create :work_time, project: project, user: user, creator: user, starts_at: Time.current - 1.day - 30.minutes, ends_at: Time.current - 1.day
+    FactoryGirl.create :work_time, project: project2, user: user, creator: user, starts_at: Time.current - 31.days - 30.minutes, ends_at: Time.current - 31.days
+    FactoryGirl.create :work_time, project: project3, user: user, creator: user, starts_at: Time.current - 61.days - 30.minutes, ends_at: Time.current - 61.days
+    login_user(user)
 
     click_link('Projects')
     expect(page).to have_content('Rank')
+    expect(page).to have_selector('select')
+    expect(page).to have_selector('select > option', count: 3)
+    expect(page).to have_selector('.grid.ui > .card.column.five.wide', count: 1)
 
+    aggregate_failures 'properly filter' do
+      find('select > option', text: 'Last 60 days').click
+      expect(page).to have_selector('.grid.ui > .card.column.five.wide', count: 2)
+    end
+
+    aggregate_failures 'properly filter' do
+      find('select > option', text: 'Last 90 days').click
+      expect(page).to have_selector('.grid.ui > .card.column.five.wide', count: 3)
+    end
+
+    aggregate_failures 'properly filter' do
+      find('select > option', text: 'Last 30 days').click
+      expect(page).to have_selector('.grid.ui > .card.column.five.wide', count: 1)
+    end
+
+    aggregate_failures 'projects listing - active' do
+      find('a.btn.btn-default', text: 'All').click
+      expect(page).to have_selector('tbody > tr', count: 3)
+    end
+
+    aggregate_failures 'projects listing - inactive' do
+      find('select > option', text: 'Inactive').click
+      expect(page).to have_selector('tbody > tr', count: 1)
+    end
+
+    aggregate_failures 'projects listing - all' do
+      find('select > option', text: 'All').click
+      expect(page).to have_selector('tbody > tr', count: 4)
+    end
+  end
+
+  it 'Profile' do
+    user = FactoryGirl.create :user, lang: 'en', password: 'password'
+    login_user(user)
     click_link("#{user.last_name} #{user.first_name}")
-    expect(page).to have_selector('input')
+    expect(page).to have_selector('input[name="first_name"]')
+    expect(page).to have_selector('input[name="last_name"]')
+    expect(page).to have_selector('select[name="lang"]')
 
-    click_link('Sign out')
-    expect(page).to have_content('Zaloguj')
+    aggregate_failures 'Can edit properies' do
+      within('form') do
+        fill_in 'first_name', with: 'John'
+        fill_in 'last_name', with: 'Doe'
+        click_button 'Save'
+      end
+
+      expect(page).to have_content('Rank')
+      user.reload
+      expect(user.first_name).to eq 'John'
+      expect(user.last_name).to eq 'Doe'
+    end
   end
 end
