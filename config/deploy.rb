b75ce0a181aabaa94ffb97b06c56bc3788e5199b
Kamil Baćkowski <kbackowski@gmail.com>
2018-10-11 09:39:10 +0200

    Update capfile lock to 3.11.0

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.9.0'
+lock '3.11.0'
 
 set :application, 'timetable'
 set :bundle_bins, fetch(:bundle_bins, []).concat(%w[sidekiq sidekiqctl])
