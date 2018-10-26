83e1030f059cbbed68d9527950e6af91d14f94d4
Kamil Baćkowski <kbackowski@gmail.com>
2018-10-26 10:01:14 +0200

    Deploy to staging

---
+++ config/deploy/staging.rb
@@ -0,0 +1,4 @@
+set :rails_env, :staging
+set :deploy_to, ENV['CAP_STAGING_DEPLOY_TO']
+server ENV['CAP_STAGING_SERVER'], user: ENV['CAP_STAGING_SSH_USER'], roles: %w[app db web]
+set :rvm_ruby_version, 'ruby-2.4.1@leaderboard'
