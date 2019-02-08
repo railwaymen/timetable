f37b9a6ffabf21c3e587e07328e8e4abb310ef61
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2019-02-08 09:10:40 +0100

    Fix warns in edit form

--- app/javascript/components/shared/helpers.js
+++ app/javascript/components/shared/helpers.js
@@ -1,3 +1,14 @@
 import React from 'react';
 
-export const preserveLines = string => `${string}`.split("\n").map((line, idx) => <p key={idx}>{line}</p>)
+export const preserveLines = string => `${string}`.split("\n").map((line, idx) => <p key={idx}>{line}</p>);
+export const unnullifyFields = (object) => {
+  const result = Object.create(null);
+  for (let field in object) {
+    if (object[field] === null) {
+      result[field] = '';
+    } else {
+      result[field] = object[field]
+    }
+  }
+  return result;
+};
