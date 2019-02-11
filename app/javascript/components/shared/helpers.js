eb28853ba0fa9b2d96629d1818570bae92561f08
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2019-02-11 10:23:56 +0100

    Split map to multiple lines

--- app/javascript/components/shared/helpers.js
+++ app/javascript/components/shared/helpers.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
-export const preserveLines = string => String(string).split("\n").map((line, idx) => <p key={idx}>{line}</p>);
+export const preserveLines = string => String(string).split("\n").map((line, idx) => (
+  <p key={idx}>{line}</p>
+));
+
 export const unnullifyFields = (object) => {
   const result = {};
   for (let field in object) {
