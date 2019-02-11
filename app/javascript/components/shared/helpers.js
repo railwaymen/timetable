82bfc3aaf046cc5d093465d3b2d95d6d9822f88e
Marcin Henryk Bartkowiak <mhbartkowiak@gmail.com>
2019-02-11 10:00:34 +0100

    Use {} instead of Object.create

--- app/javascript/components/shared/helpers.js
+++ app/javascript/components/shared/helpers.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const preserveLines = string => String(string).split("\n").map((line, idx) => <p key={idx}>{line}</p>);
 export const unnullifyFields = (object) => {
-  const result = Object.create(null);
+  const result = {};
   for (let field in object) {
     if (object[field] === null) {
       result[field] = '';
