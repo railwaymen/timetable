df0657ec875cde66473b390c23126e041d244f8a
Kamil Backowski <kbackowski@gmail.com>
2020-02-24 15:34:41 +0100

    Use new ruby version on staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,5 +5,5 @@ set :nvm_node, 'v11.4.0'
 set :rails_env, :staging
 set :deploy_to, ENV['CAP_STAGING_DEPLOY_TO']
 server ENV['CAP_STAGING_SERVER'], user: ENV['CAP_STAGING_SSH_USER'], roles: %w[app db web]
-set :rvm_ruby_version, 'ruby-2.4.1@leaderboard'
+set :rvm_ruby_version, 'ruby-2.7.0@leaderboard'
 set :branch, :develop
