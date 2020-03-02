92a040751983b638d452ee708e816a28c70f37a8
Piotr Sobon <piotrsobon86@gmail.com>
2020-03-02 16:00:12 +0100

    Changed project components to functional

--- app/javascript/components/time_table/projects/project_stats.js
+++ app/javascript/components/time_table/projects/project_stats.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import { Link } from 'react-router-dom';
 
-class ProjectStats extends React.Component {
-  projectLabel() {
-    const data = this.props.stats[0];
+function ProjectStats(props) {
+  function projectLabel() {
+    const data = props.stats[0];
     if (currentUser.canManageProject({ id: data.project_id })) {
       return (
         <Link to={`/projects/${data.project_id}/work_times`}>
@@ -15,36 +16,38 @@ class ProjectStats extends React.Component {
     return data.name;
   }
 
-  render() {
-    const { stats } = this.props;
-    const data = stats[0];
+  const { stats } = props;
+  const data = stats[0];
 
-    return (
-      <div className="col-xs-12 col-sm-6 col-lg-4 card-container project-card">
-        <div className="card h-100">
-          <h3 className="title">
-            {this.projectLabel()}
-            <div
-              className="badge"
-              style={{
-                backgroundColor: `#${data.color}`, width: '18px', height: '18px', display: 'block',
-              }}
-            />
-          </h3>
-          <p className="text-center">
-            {data.leader ? data.leader.name : ''}
-          </p>
-          <ul>
-            { stats.map((stat) => (
-              <li className="person" key={stat.id}>
-                {stat.user.name}
-              </li>
-            )) }
-          </ul>
-        </div>
+  return (
+    <div className="col-xs-12 col-sm-6 col-lg-4 card-container project-card">
+      <div className="card h-100">
+        <h3 className="title">
+          {projectLabel()}
+          <div
+            className="badge"
+            style={{
+              backgroundColor: `#${data.color}`, width: '18px', height: '18px', display: 'block',
+            }}
+          />
+        </h3>
+        <p className="text-center">
+          {data.leader ? data.leader.name : ''}
+        </p>
+        <ul>
+          { stats.map((stat) => (
+            <li className="person" key={stat.id}>
+              {stat.user.name}
+            </li>
+          )) }
+        </ul>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+ProjectStats.propTypes = {
+  stats: PropTypes.array.isRequired,
+};
 
 export default ProjectStats;
