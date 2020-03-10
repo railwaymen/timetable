4afc1780dba09495e9bedae8c01d60d4a626e816
Kamil Backowski <kbackowski@gmail.com>
2020-03-10 12:19:00 +0100

    Fixed tests

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,6 @@ module TimeTable
     config.action_mailer.default_url_options = { host: Rails.application.secrets.mailer[:host] }
     config.action_mailer.delivery_method = :sendmail
     config.action_mailer.default_options = { from: Rails.application.secrets.mailer[:from] }
-    config.hosts << Rails.application.secrets[:host]
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,6 +16,8 @@ Rails.application.configure do # rubocop:disable Metrics/BlockLength
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  config.hosts << Rails.application.secrets[:host]
+
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
