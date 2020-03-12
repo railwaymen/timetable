5dffb3f3dba29ebfa3ab926c0bb39d9463529ddd
Kamil Backowski <kbackowski@gmail.com>
2020-03-12 11:13:14 +0100

    Upgrade sidekiq

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,3 +7,4 @@ set :deploy_to, ENV['CAP_STAGING_DEPLOY_TO']
 server ENV['CAP_STAGING_SERVER'], user: ENV['CAP_STAGING_SSH_USER'], roles: %w[app db web]
 set :rvm_ruby_version, 'ruby-2.7.0@leaderboard'
 set :branch, :develop
+set :sidekiq_systemd_name, 'tt_sidekiq'
