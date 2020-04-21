5b1fb80b09bdc8922b5032a60d87cd4bf3a366f1
Kamil Backowski <kbackowski@gmail.com>
2020-04-21 13:15:55 +0200

    Add trusted hosts configuration required by rails 6

--- config/application.rb
+++ config/application.rb
@@ -17,6 +17,8 @@ module TimeTable
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.hosts.push *ENV['TRUSTED_HOSTS'].split(',')
+
     config.autoload_paths += %W[#{config.root}/lib]
     config.eager_load_paths += %W[#{config.root}/lib]
 
