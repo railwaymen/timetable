d5ff2e0a3bff2e215e11a22d5cf3a00fe7ee516f
Kamil Backowski <kbackowski@gmail.com>
2020-04-21 08:45:50 +0200

    Temporarily disable deploy to production 2

--- config/deploy/production_1.rb
+++ config/deploy/production_1.rb
@@ -3,5 +3,5 @@
 set :rails_env, :production
 set :deploy_to, ENV['CAP_DEPLOY_TO_1']
 server ENV['CAP_SERVER_1'], user: ENV['CAP_SSH_USER_1'], roles: %w[app db web]
-set :rvm_ruby_version, 'ruby-2.4.1@leaderboard'
+set :rvm_ruby_version, 'ruby-2.7.0@leaderboard'
 set :sidekiq_systemd_name, 'tt_sidekiq'
