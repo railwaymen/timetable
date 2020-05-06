dd8bfd5cdd09c758c53c98d65883a219d8c9a4b7
Jan Jagieła <janjag@humanistcanco.de>
2020-05-06 13:51:52 +0200

    updated styling of raport cards

--- app/javascript/components/time_table/reports/report_project_record.js
+++ app/javascript/components/time_table/reports/report_project_record.js
@@ -13,32 +13,34 @@ function ReportProjectRecord(props) {
   const overallData = reportRows[0];
 
   return (
-    <div className="panel panel-default">
-      <div className="panel-heading">
-        <h4>
-          <span className="badge">{formattedDuration(overallData.project_duration)}</span>
+    <div className="card p-0 w-100">
+      <div className="card-header">
+        <h4 className="row mx-0">
           {overallData.project_name}
-          <a href={`/reports/project.csv?from=${from}&to=${to}&id=${overallData.project_id}`}>
-            <i className="calendar icon" />
+          <a className="mx-2" href={`/reports/project.csv?from=${from}&to=${to}&id=${overallData.project_id}`}>
+            <i className="fa fa-calendar" />
           </a>
+          <span className="ml-auto badge badge-dark">{formattedDuration(overallData.project_duration)}</span>
         </h4>
       </div>
-      <ul className="list-group">
-        {reportRows.map((row) => (
-          <li className="list-group-item record" key={row.user_id}>
-            <a
-              href={`/timesheet?project_id=${row.project_id}&user_id=${row.user_id}&from=${from}&to=${to}`}
-              onClick={onRedirect}
-            >
-              {row.user_name}
-            </a>
-            (
-            {countDurationPercentage(row.duration, row.project_duration)}
-            )
-            <span className="badge badge-dark ml-auto">{formattedDuration(row.duration)}</span>
-          </li>
-        ))}
-      </ul>
+      <div className="card-body">
+        <ul className="list-group">
+          {reportRows.map((row) => (
+            <li className="list-group-item record" key={row.user_id}>
+              <a
+                href={`/timesheet?project_id=${row.project_id}&user_id=${row.user_id}&from=${from}&to=${to}`}
+                onClick={onRedirect}
+              >
+                {row.user_name}
+              </a>
+              (
+              {countDurationPercentage(row.duration, row.project_duration)}
+              )
+              <span className="badge badge-dark ml-auto">{formattedDuration(row.duration)}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- app/javascript/components/time_table/reports/report_user_record.js
+++ app/javascript/components/time_table/reports/report_user_record.js
@@ -19,30 +19,32 @@ class ReportUserRecord extends React.Component {
 
     return (
       <div className="col-12 col-md-4">
-        <div className="card panel-default">
-          <div className="panel-heading">
-            <h4>
+        <div className="card p-0 w-100">
+          <div className="card-header">
+            <h4 className="row align-items-baseline mx-0">
               <i className="fa fa-user" />
-              <a href={`/timesheet?user_id=${overallData.user_id}&from=${from}&to=${to}`} onClick={this.onRedirect}>{overallData.user_name}</a>
+              <a  className="mx-2" href={`/timesheet?user_id=${overallData.user_id}&from=${from}&to=${to}`} onClick={this.onRedirect}>{overallData.user_name}</a>
               {currentUser.isAdminOrManagerOrLeader() && (
-                <a href={`/reports/project.csv?from=${from}&to=${to}&user_id=${overallData.user_id}`}><i className="calendar icon" /></a>
+                <a href={`/reports/project.csv?from=${from}&to=${to}&user_id=${overallData.user_id}`}><i className="fa fa-calendar" /></a>
               )}
-              <span className="badge">{formattedDuration(overallData.user_work_time)}</span>
+              <span className="ml-auto badge badge-dark">{formattedDuration(overallData.user_work_time)}</span>
             </h4>
           </div>
-          <ul className="list-group">
-            { reportRows.map((row) => (
-              <li className="list-group-item" key={row.project_id}>
-                <a href={`/timesheet?project_id=${row.project_id}&user_id=${row.user_id}&from=${from}&to=${to}`} onClick={this.onRedirect}>
-                  {row.project_name}
-                </a>
-                (
-                {countDurationPercentage(row.time_worked, row.user_work_time)}
-                )
-                <span className="badge badge-dark ml-auto">{formattedDuration(row.time_worked)}</span>
-              </li>
-            )) }
-          </ul>
+          <div className="card-body">
+            <ul className="list-group">
+              { reportRows.map((row) => (
+                <li className="list-group-item" key={row.project_id}>
+                  <a href={`/timesheet?project_id=${row.project_id}&user_id=${row.user_id}&from=${from}&to=${to}`} onClick={this.onRedirect}>
+                    {row.project_name}
+                  </a>
+                  (
+                  {countDurationPercentage(row.time_worked, row.user_work_time)}
+                  )
+                  <span className="badge badge-dark ml-auto">{formattedDuration(row.time_worked)}</span>
+                </li>
+              )) }
+            </ul>
+          </div>
         </div>
       </div>
     );
