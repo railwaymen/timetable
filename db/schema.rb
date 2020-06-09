adfdb71c6724bcc261295bf07135645ec1a27c26
Kamil Backowski <kbackowski@gmail.com>
2020-06-09 10:17:33 +0200

    Remove unused _contract_name from work_times. Fixed specs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_104822) do
+ActiveRecord::Schema.define(version: 2020_06_09_071858) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -366,7 +366,6 @@ ActiveRecord::Schema.define(version: 2020_06_05_104822) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "_contract_name"
     t.integer "creator_id", null: false
     t.boolean "updated_by_admin", default: false, null: false
     t.string "task"
