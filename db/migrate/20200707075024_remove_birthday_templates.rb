f390d981112cf0139477fe63fcc5f56410913ce5
Kamil Backowski <kbackowski@gmail.com>
2020-07-08 09:10:43 +0200

    Make last migration reversible

--- db/migrate/20200707075024_remove_birthday_templates.rb
+++ db/migrate/20200707075024_remove_birthday_templates.rb
@@ -1,6 +1,13 @@
 class RemoveBirthdayTemplates < ActiveRecord::Migration[6.0]
   def change
-    drop_table :birthday_email_templates
-    remove_column :users, :birthdate
+    drop_table :birthday_email_templates do |t|
+      t.text :body, null: false
+      t.string :name, null: false
+      t.string :title, null: false
+      t.boolean :last_used, null: false, default: false
+      
+      t.timestamps
+    end
+    remove_column :users, :birthdate, :date
   end
 end
