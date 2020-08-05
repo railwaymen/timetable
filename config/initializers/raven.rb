ef41a34d780d5a2eaab6fb8dbc8b146e2852fb7c
Kamil Backowski <kbackowski@gmail.com>
2020-08-05 13:16:47 +0200

    Capture real post data in sentry exceptions

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -5,5 +5,6 @@ if Rails.application.secrets.raven_url.present?
     config.dsn = Rails.application.secrets.raven_url
     config.environments = %w[production staging demo]
     config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
+    config.processors -= [Raven::Processor::PostData]
   end
 end
