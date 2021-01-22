0b1f39a0ab98d2d620b9a5d21b96adaef4f6db6c
Kamil Backowski <kbackowski@gmail.com>
2021-01-22 07:56:05 +0100

    Do not raise exception when there is no current vacation period

--- app/controllers/api/vacations_controller.rb
+++ app/controllers/api/vacations_controller.rb
@@ -16,7 +16,7 @@ module Api
         end
       @vacations = Vacation.where('user_id = :user_id AND (extract(year from start_date) = :year OR extract(year from start_date) = :year)',
                                   user_id: selected_user.id, year: params[:year]).order(:start_date)
-      vacation_period = selected_user.vacation_periods.where('extract(year from starts_at) = ?', params[:year]).first!
+      vacation_period = selected_user.vacation_periods.where('extract(year from starts_at) = ?', params[:year]).first
       @available_vacation_days = selected_user.available_vacation_days(@vacations, vacation_period)
       @used_vacation_days = selected_user.used_vacation_days(@vacations)
     end
