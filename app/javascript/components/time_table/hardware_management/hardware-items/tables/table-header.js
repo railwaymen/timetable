65076e06d58079653df3bfb9a7113a6921eb57cc
Wojciech Łaciak <wojciech.laciak@railwaymen.org>
2021-12-13 14:45:28 +0100

    Removed operating system column from hardware list

--- app/javascript/components/time_table/hardware_management/hardware-items/tables/table-header.js
+++ app/javascript/components/time_table/hardware_management/hardware-items/tables/table-header.js
@@ -7,7 +7,6 @@ export default function TableHeader() {
         <th>{I18n.t('apps.hardware_devices.category')}</th>
         <th>{I18n.t('apps.hardware_devices.brand')}</th>
         <th>{I18n.t('apps.hardware_devices.model')}</th>
-        <th>{I18n.t('apps.hardware_devices.os_version')}</th>
         <th>{I18n.t('apps.hardware_devices.serial_number')}</th>
         <th>{I18n.t('apps.hardware_devices.assigned_person')}</th>
         <th>{I18n.t('apps.hardware_devices.state')}</th>
