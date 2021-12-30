262a94e01832196e447c34687e632804cadc57e1
Wojciech Łaciak <skonry1604@gmail.com>
2021-12-30 21:39:36 +0100

    Fixed table layout

--- app/javascript/components/time_table/vacation_periods/vacation_period.js
+++ app/javascript/components/time_table/vacation_periods/vacation_period.js
@@ -13,19 +13,17 @@ function VacationPeriod(props) {
       <td>{period.ends_at}</td>
       <td>{period.vacation_days}</td>
       <td className="text-left">{preserveLines(period.note || '')}</td>
-      <td className="nowrap text-left">
-        { currentUser.admin && (
-          <>
-            <NavLink
-              to={`/vacation_periods/edit/${period.id}?user_id=${period.user_id}`}
-              className="btn btn-outline-secondary edit"
-            >
-              <i className="fa fa-pencil mr-2" />
-              {I18n.t('common.edit')}
-            </NavLink>
-          </>
-        )}
-      </td>
+      { currentUser.admin && (
+        <td className="nowrap text-left">
+          <NavLink
+            to={`/vacation_periods/edit/${period.id}?user_id=${period.user_id}`}
+            className="btn btn-outline-secondary edit"
+          >
+            <i className="fa fa-pencil mr-2" />
+            {I18n.t('common.edit')}
+          </NavLink>
+        </td>
+      )}
     </tr>
   );
 }
